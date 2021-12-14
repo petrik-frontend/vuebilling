3c12eb2821874e94fc31d588461aeb919270a8bb
Your Name <you@example.com>
2021-12-14 14:43:24 +0100

    Egy lehetséges megoldás

--- src/App.vue
+++ src/App.vue
@@ -1,20 +1,74 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <table border="1">
+      <thead>
+        <tr>
+          <th>Név</th>
+          <th>Ár (Ft)</th>
+          <th>DB</th>
+          <th>Operations</th>
+        </tr>
+      </thead>
+      <tbody>
+        
+        <tr v-for="row in rows" v-bind:key="row.title">
+          <td v-if="row.title != editedRow.old.title">{{ row.title }}</td>
+          <td v-if="row.title != editedRow.old.title">{{ row.price }}</td>
+          <td v-if="row.title != editedRow.old.title">{{ row.quantity }}</td>
+          <td v-if="row.title != editedRow.old.title">
+            <button @click="DeleteRow(row.title)">X</button>
+            <button @click="EditRow(row)">Edit</button>
+          </td>
+          <td v-if="row.title == editedRow.old.title">
+            <input type="text" v-model="editedRow.new.title">
+          </td>
+          <td v-if="row.title == editedRow.old.title">
+            <input type="number" v-model="editedRow.new.price">
+          </td>
+          <td v-if="row.title == editedRow.old.title">
+            <input type="number" v-model="editedRow.new.quantity">
+          </td>
+          <td v-if="row.title == editedRow.old.title">
+            <button @click="SaveRow">Save</button>
+          </td>
+        </tr>
+
+        <tr>
+          <td><input type="text" v-model="newRow.title"></td>
+          <td><input type="number" v-model="newRow.price"></td>
+          <td><input type="number" v-model="newRow.quantity"></td>
+          <td><button @click="AddNew">Add</button></td>
+        </tr>
+      </tbody>
+    </table>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
   },
   data() {
     return {
+      newRow: {
+        title: '',
+        price: null,
+        quantity: null
+      },
+      editedRow: {
+        old: {
+          title: '',
+          price: null,
+          quantity: null
+        }, 
+        new: {
+          title: '',
+          price: null,
+          quantity: null
+        } 
+      },
       rows: [
         {
           title: 'Kerék',
@@ -37,6 +91,30 @@ export default {
           quantity: 321
         },
       ]
+    }
+  },
+  methods: {
+    AddNew () {
+      this.rows.push({... this.newRow})
+    },
+    DeleteRow(title) {
+      this.rows = this.rows.filter(function (item){
+        return item.title != title
+      })
+    },
+    EditRow(editedRow) {
+      this.editedRow.old = {...editedRow}
+      this.editedRow.new = {...editedRow}
+    },
+    SaveRow () {
+      let editedRow = {...this.editedRow}
+      this.rows = this.rows.map(function (item) {
+        if (item.title != editedRow.old.title) {
+          return item
+        }
+        return {...editedRow.new}
+      })
+      this.editedRow.old.title = ''
     }
   }
 }
